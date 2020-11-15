dbd87decf8be3727b80bfb877194bed782769aec
Poorni Amaratunga <poorniamarathunga1997@gmail.com>
2020-11-15 13:08:38 +0530

    Add form validation

--- js/customer-ctrl.js
+++ js/customer-ctrl.js
@@ -41,8 +41,61 @@ function init(){
 
 // Todo: add all event listeners and handlers here
 
-/*===============================================================================
- * Functions
- *===============================================================================*/
+var saveBtn = document.getElementById('btn-save');
+saveBtn.addEventListener('click',validateFields);
+
+function validateFields(){
+    var id = document.getElementById('txt-id').value;
+    var name = document.getElementById('txt-name').value;
+    var address = document.getElementById('txt-address').value;
+
+    var txtId = document.getElementById('txt-id');
+    var txtName = document.getElementById('txt-name');
+    var txtAddress = document.getElementById('txt-address');
+
+    var validId = false;
+    var validName = false;
+    var validAddress = false;
+
+    if(id.length === 0){
+        txtId.style.border = '2px solid red';
+        return;
+    }else if(id.length < 3){
+        txtId.style.border = '2px solid red';
+        return;
+    }else{
+        txtId.style.border = '1px solid #ced4da';
+        validId = true;
+    }
+
+    if(name.length === 0){
+        txtName.style.border = '2px solid red';
+        return;
+    }else if(name.length < 3){
+        txtName.style.border = '2px solid red';
+        return;
+    }else{
+        txtName.style.border = '1px solid #ced4da';
+        validName = true;
+    }
+
+    if(address.length === 0){
+        txtAddress.style.border = '2px solid red';
+        return;
+    }else if(address.length < 4){
+        txtAddress.style.border = '2px solid red';
+        return;
+    }else{
+        txtAddress.style.border = '1px solid #ced4da';
+        validAddress = true;
+    }
+
+    if(validId || validName || validAddress){
+      alert("Wade Hari kelle");
+    }
+}
 
 // Todo: add all functions
+window.onload = function() {
+    document.getElementById("txt-id").focus();
+};
